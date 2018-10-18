9d9d385316669c1dc9552b1a608ee3fec7c47e76
Hien-Chanh Quach <hcquach@gmail.com>
2018-10-18 17:49:25 +0200

    Cleaning Sequelize and PG to switch to Mongoose and MangoDB

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
-
-const models = {
-    User: sequelize.import('./user'),
-    Message: sequelize.import('./message'),
-};
