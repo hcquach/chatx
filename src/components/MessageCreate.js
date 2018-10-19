0a1c6d091538ce753175ff5d845096d2503fdf41
Hien-Chanh Quach <hcquach@gmail.com>
2018-10-19 09:31:36 +0200

    Updated form, eventhandler to successfully create a new message

--- src/components/MessageCreate.js
+++ src/components/MessageCreate.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
-import { Link, hashHistory } from 'react-router';
 
 class MessageCreate extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            title: '', 
+            content: '', 
             user: ''
         };
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        console.log(this.props);
+        this.props.mutate({
+            variables: { content: this.state.content, user: this.state.user }
+        });
     }
 
     render() {
         return (
             <div>
                 <h3>Send a new message</h3>
-                <form>
+                <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Name:</label>
                     <input 
-                        onChange={event => this.setState({ name: event.target.value })}
-                        value={this.state.name}
+                        onChange={event => this.setState({ user: event.target.value })}
+                        value={this.state.user}
                     />
                     <label>Message:</label>
                     <input 
-                        onChange={event => this.setState({ name: event.target.value })}
+                        onChange={event => this.setState({ content: event.target.value })}
                         value={this.state.content}
                     />
+                     <input type="submit" value="Submit" />
                 </form>
             </div>
         );
@@ -34,7 +42,7 @@ class MessageCreate extends Component {
 }
 
 const mutation = gql`
-    mutation AddMessage($user: String, $content: String) {
+    mutation AddMessage($user: String, $content: String!) {
         addMessage(user: $user, content: $content) {
             content
         }
